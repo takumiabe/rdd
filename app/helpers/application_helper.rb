e223c91cd423bf4fcd17b4bfa445bc075cbeb3de
Takumi Abe <abe@engraphia.com>
2017-11-22 14:28:06 +0900

    ドメイン用語のリンク化

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def decorate(str)
+    DomainModel.all.pluck(:name, :id).sort_by{|name| -name.size }.each do |name, id|
+      b = content_tag(:b, link_to(name, domain_model_path(id)))
+      str = str.gsub(name, b)
+    end
+    simple_format str
+  end
 end
