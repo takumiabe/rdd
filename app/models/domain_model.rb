2fa719c9825f81b1a9be0e6f4b575ce2d23e84b9
Takumi Abe <abe@engraphia.com>
2017-11-22 14:49:24 +0900

    ドメインモデルに出現箇所を表示

--- app/models/domain_model.rb
+++ app/models/domain_model.rb
@@ -1,2 +1,10 @@
 class DomainModel < ApplicationRecord
+
+  def requirements
+    [
+      Requirement.where('condition like ?', "%#{name}%"),
+      Requirement.where('description like ?', "%#{name}%"),
+      Requirement.where('remark like ?', "%#{name}%")
+    ].inject(:or)
+  end
 end
